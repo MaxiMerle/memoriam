456f40cdc9f8411077004725655f1c6ec1171cde
Madhi Khalfaoui <m.khalfaoui@eleria.fr>
2019-06-14 18:52:31 +0200

    a terminer lundi

---
+++ src/Controller/UploadController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Projet;
+use App\Entity\ProjetClient;
+use App\Entity\ProjetClientCategorie;
+use App\Entity\ProjetClientQualite;
+use App\Entity\ImageFiles;
+use App\Form\ProjetClientType;
+
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class UploadController extends AbstractController
+{
+    /**
+     * @Route("/fileuploadhandler", name="fileuploadhandler")
+     */
+    public function fileUploadHandler(Request $request)
+    {
+        $output = array('uploaded' => false);
+        // get the file from the request object
+        $file = $request->files->get('file');
+        $commentaire=$request->get('commentaire');
+        $projet_id=$request->get('projetId');
+        $fileName = $file->getClientOriginalName();//$file->guessExtension()
+        $uploadDir = $this->getParameter('kernel.project_dir') . '/../web/uploads/';
+        if (!file_exists($uploadDir) && !is_dir($uploadDir)) {
+            mkdir($uploadDir, 0775, true);
+        }
+        if ($file->move($uploadDir, $fileName)) {
+            $em = $this->getDoctrine()->getManager();
+            $mediaEntity = new ImageFiles();
+            $mediaEntity->setNomFichier($fileName);
+            $mediaEntity->setCommentaire($commentaire);
+            //$mediaEntity->setProjet($em->find("ProjetClient"), $projet_id);
+            // sauvegarde
+            $em->persist($mediaEntity);
+            $em->flush();
+            $output['uploaded'] = true;
+            $output['fileName'] = $fileName;
+        }
+        return new JsonResponse($output);
+    }
+}
---
+++ src/Entity/ImageFiles.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ImageFilesRepository")
+ * @ORM\Table(name="image_files")
+ */
+class ImageFiles
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $nomFichier;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\ProjetClient", inversedBy="projetFiles")
+     */
+    private $projet;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $commentaire;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNomFichier(): ?string
+    {
+        return $this->nomFichier;
+    }
+
+    public function setNomFichier(?string $nomFichier): self
+    {
+        $this->nomFichier = $nomFichier;
+
+        return $this;
+    }
+
+
+    public function getCommentaire(): ?string
+    {
+        return $this->commentaire;
+    }
+
+    public function setCommentaire(?string $commentaire): self
+    {
+        $this->commentaire = $commentaire;
+
+        return $this;
+    }
+
+
+    public function getProjet(): ?ProjetClient
+    {
+        return $this->projet;
+    }
+
+    public function setProjet(?ProjetClient $projet): self
+    {
+        $this->projet = $projet;
+
+        return $this;
+    }
+}
